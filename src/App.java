08a1a16cef4b2f263e9dc3e333cf7456f5d3b88a
Andre050101 <agiancristiano90@gmail.com>
2025-03-18 13:33:18 +0100

    oop shop con incapsulamento

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
+import java.math.BigDecimal;
+
 import org.lessons.java.shop.Prodotto;
 public class App {
     public static void main(String[] args) throws Exception {
-        Prodotto Giocattolo = new Prodotto("Peluche", "Peluche morbidoso", 11.69f, 12);
-        System.out.println("Il prezzo senza iva è "+Giocattolo.getPrice());
+        Prodotto Giocattolo = new Prodotto("Peluche", "Ciao morbidoso", new BigDecimal(10), new BigDecimal(12));
+        System.out.println("Il prezzo senza iva è "+Giocattolo.getPrezzo());
         System.out.println("Il prezzo con iva è "+Giocattolo.getPriceWIva());
         System.out.println("Il nome esteso con nome e codice è "+Giocattolo.getExtendedName());
+        System.out.println("Dati: codice: "+Giocattolo.getCodice()+", nome: "+Giocattolo.getNome()+", descrizione: "+Giocattolo.getDescrizione()+", prezzo: "+Giocattolo.getPrezzo()+", iva: "+Giocattolo.getIva());
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -5,12 +5,22 @@ import java.math.RoundingMode;
 
 
 public class Prodotto {
-    public int codice;
-    public String nome;
-    public String descrizione;
-    public BigDecimal prezzo;
-    public BigDecimal iva;
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private BigDecimal prezzo;
+    private BigDecimal iva;
 
+    //Costruttore senza parametri
+    public Prodotto(){
+        Random rand = new Random();
+        this.codice = rand.nextInt(99999);
+        this.nome = "Prodotto senza nome";
+        this.descrizione = "No descrizione";
+        this.prezzo = new BigDecimal(0);
+        this.iva = new BigDecimal(0);
+    }
+    //Costruttore con tutti i valori passati
     public Prodotto(String nome, String descrizione, BigDecimal prezzo, BigDecimal iva){
         Random rand = new Random();
         this.codice = rand.nextInt(99999);
@@ -20,10 +30,48 @@ public class Prodotto {
         this.iva = iva;
     }
 
-    public BigDecimal getPrice(){
+    //Getter e Setter
+    public int getCodice(){
+        return this.codice;
+    }
+
+    public String getNome(){
+        if(this.nome != null)
+            return this.nome;
+        return "Prodotto senza nome";
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getDescrizione(){
+        if(this.descrizione != null)
+            return this.descrizione;
+        return "Prodotto senza descrizione";
+    }
+
+    public void setDescrizione(String descrizione){
+        this.descrizione = descrizione;
+    }
+
+    public BigDecimal getPrezzo(){
         return this.prezzo;
     }
 
+    public void setPrezzo(BigDecimal prezzo){
+        this.prezzo = prezzo;
+    }
+
+    public BigDecimal getIva(){
+        return this.iva;
+    }
+
+    public void setIva(BigDecimal iva){
+        this.iva = iva;
+    }
+
+    //Other methods
     public BigDecimal getPriceWIva(){
         if(prezzo != null && iva != null){
             return prezzo.add(prezzo.multiply(iva)).setScale(2, RoundingMode.DOWN);
